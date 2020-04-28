87e325ff3fb5917483826779794a052a5fd344a1
RAJAT RASTOGI <32540654+rajatr28@users.noreply.github.com>
2020-04-29 02:02:33 +0530

    Add files via upload

---
+++ labbook2/src/main/java/com/cg/labbook2/trainee/service/UserService.java
@@ -0,0 +1,22 @@
+package com.cg.labbook2.trainee.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.labbook2.trainee.dao.UserDao;
+import com.cg.labbook2.trainee.entity.User;
+
+@Service
+@Transactional
+public class UserService {
+	
+	@Autowired
+	UserDao userDao;
+	
+	public User findByUsernameAndPassword(String username, String password) {
+		return userDao.findByUsernameAndPassword(username, password);
+	}
+
+}
---
+++ labbook2/src/test/java/com/cg/labbook2/Labbook2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.labbook2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labbook2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
